4956866a7100438b587a9c1300ac26d6857e9184
eylee02 <leywindy@naver.com>
2023-07-26 17:28:11 +0900

    20230726

--- 11_CollectionFramework/src/ex01_generic/MainWrapper.java
+++ 11_CollectionFramework/src/ex01_generic/MainWrapper.java
@@ -12,6 +12,7 @@ public class MainWrapper {
     // int를 저장하는 box2
     // 오직 "참조타입"만 Generic 처리에서 사용할 수 있다.
     // int의 참조타입인 Integer를 사용한다.
+    // 참조타입 01_Varialble 프로젝트 ex01에 정리해둠
     Box<Integer> box2 = new Box<Integer>();
     box2.setItem(10);
     System.out.println(box2.getItem());
