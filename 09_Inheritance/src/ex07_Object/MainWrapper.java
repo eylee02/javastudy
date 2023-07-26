058c2366eccad29d1804f7f852b6347e20fef312
eylee02 <leywindy@naver.com>
2023-07-26 11:47:31 +0900

    20230726

--- 09_Inheritance/src/ex07_Object/MainWrapper.java
+++ 09_Inheritance/src/ex07_Object/MainWrapper.java
@@ -1,14 +1,45 @@
 package ex07_Object;
 
 public class MainWrapper {
+  
+  public static void ex01() {
 
-  public static void main(String[] args) {
+    
+    // Object 타입으로 모든 객체를 저장할 수 있다.
+    // Object 타입으로 저장한 각체를 사용할 때는 캐스팅 해야 한다.
     
     Object obj = new Person();
     ((Person)obj).setName("홍길동");
     ((Person)obj).setAge(20);
     System.out.println(((Person)obj).getName());
     System.out.println(((Person)obj).getAge());
+    
+  }
+  
+  public static void ex02() {
+    
+    // 동일한 객체 2개
+    Person p1 = new Person("홍길동", 20);
+    Person p2 = new Person("홍길동", 20);
+    
+    // 동일한 객체인지 판단
+    boolean same = p1.equals(p2);  // Object의 equals를 사용하여 p1과 p2가 같은 객체(같은주소)인지 확인
+    
+    // 결과 확인
+    System.out.println(same ? "동일한 객체" : "다른 객체");   // 결과값 다른객체
+    
+  }
+  
+  public static void ex03() {
+    
+    Person p = new Person("홍길동", 20);
+     
+    System.out.println(p);  // 객체 p는 p.toString()이 자동 호출되서 사용된다. 오버라이드 후 출력값 Person [name=홍길동, age=20]
+    
+  }
+
+  public static void main(String[] args) {
+    ex03();
 
   }
 
--- 09_Inheritance/src/ex07_Object/Person.java
+++ 09_Inheritance/src/ex07_Object/Person.java
@@ -1,11 +1,39 @@
 package ex07_Object;
 
+import java.util.Objects;
+
 /*
- * Object 클래스
+ * 패키지명[java.lang] 클래스명[Object]
+ * java.lang.Object 클래스
  * 1. 모든 클래스의 최상위 슈퍼 클래스이다.
  * 2. 별도의 슈퍼 클래스를 명시하지 않은 클래스들은(extends가 없는 클래스) 모두 Object 클래스의 서브 클래스이다.
  * 3. 모든 것을 저장할 수 있는 Java의 만능 타입이다.
  * 4. Object 타입으로 저장하면 Object 클래스의 메소드만 호출할 수 있다. 이를 해결하기 위해서 "반드시" 캐스팅을 해야 한다.
+ */
+
+/*
+ *  ┌--------------┐
+ *  │    Object    │ 슈퍼 클래스
+ *  │--------------│
+ *  │  equals()    │ 두 객체의 참조값을 비교해서 같으면 true, 아니면 false를 반환한다.
+ *  │  getClass()  │ 어떤 클래스인지 반환
+ *  │  hashCode()  │ int 타입의 해시코드값, Object 클래스는 객체의 참조값을 해시코드값으로 사용함 
+ *  │  toString()  │ "클래스이름@참조값" 형식의 문자열을 반환 (결과값예시 ex07_Object.Person@33a49db)
+ *  │  notify()    │ 스레드(thread) 관련 메소드
+ *  │  wait()      │ 스레드(thread) 관련 메소드
+ *  └--------------┘
+ *          ▲
+ *          │
+ *          │
+ *  ┌--------------┐
+ *  │    Person    │ 서브 클래스
+ *  │--------------│
+ *  │  @Override   │ 모든 객체 비교는 equals 메소드를 사용
+ *  │  equals()    │ 이름과 나이가 같으면 true, 아니면 false 반환한다. (하드코딩하지 않고, 자동완성한다.)
+ *  │              │
+ *  │  @Override   │ 모든 객체 출력은 toString 메소드를 사용
+ *  │  toString()  │ 이름과 나이를 확인할 수 있는 문자열 반환 (하드코딩하지 않고, 자동완성한다.)
+ *  └--------------┘ 
  */
 
 public class Person {
@@ -22,7 +50,33 @@ public class Person {
     this.name = name;
     this.age = age;   
   }
-
+  
+  
+  
+  // Source 메뉴로 자동 Override
+  @Override
+  public int hashCode() {
+    return Objects.hash(age, name);
+  }
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)  // p1.equals(p1)
+      return true;
+    if (obj == null)  // p1.equals(null)
+      return false;
+    if (getClass() != obj.getClass())  // p1.equals(s1)  클래스가다른경우
+      return false;
+    Person other = (Person) obj;
+    return age == other.age && Objects.equals(name, other.name);
+  }
+  
+  // toString 자동 오버라이드 
+  @Override
+  public String toString() {
+    return "Person [name=" + name + ", age=" + age + "]";
+  }
+  
+  
   public String getName() {
     return name;
   }
@@ -37,8 +91,6 @@ public class Person {
 
   public void setAge(int age) {
     this.age = age;
-  }
-  
-  
+  }    
 
 }
