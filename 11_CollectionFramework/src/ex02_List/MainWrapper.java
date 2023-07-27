525f89e2797ba893275b75fa7556e1d224e6aec0
eylee02 <leywindy@naver.com>
2023-07-27 11:28:15 +0900

    20230727

--- 11_CollectionFramework/src/ex02_List/MainWrapper.java
+++ 11_CollectionFramework/src/ex02_List/MainWrapper.java
@@ -1,7 +1,32 @@
 package ex02_List;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;     // java.util 패키지의 List 클래스
+
+/*
+ *  ┌--------------┐
+ *  │     List     │ List 인터페이스
+ *  │--------------│
+ *  │   add();     │
+ *  │   get();     │
+ *  │   size();    │
+ *  │   set();     │
+ *  │   remove();  │
+ *  └--------------┘
+ *          ▲
+ *          │
+ *          │
+ *  ┌--------------┐
+ *  │   ArrayList  │ ArrayList 클래스
+ *  │--------------│
+ *  │   add(){}    │ 요소 추가하기
+ *  │   get(){}    │ 요소 가져오기
+ *  │   size(){}   │ 요소 개수 반환하기
+ *  │   set(){}    │ 요소 수정하기
+ *  │   remove(){} │ 요소 삭제하기
+ *  └--------------┘
+ */
 
 public class MainWrapper {
   
@@ -54,9 +79,63 @@ public class MainWrapper {
     
   }  
   
+  public static void ex03() {
+    
+    // ArrayList의 선언 & 생성
+    List<String> flower = new ArrayList<String>();
+    
+    // 요소 추가하기
+    flower.add("장미");
+    flower.add("튤립");
+    flower.add("백합"); 
+    
+    // 요소 수정하기
+    flower.set(0, "백일홍");  // 인덱스 0의 요소를 "백일홍"으로 수정
+    
+    // 요소 삭제하기
+    flower.remove(1);  // 인덱스 1의 요소를 삭제
+    
+    // 요소 확인
+    for(int i = 0, length = flower.size(); i < length; i++) {
+      System.out.println(flower.get(i));
+
+    }
+  }
+    
+  public static void ex04() {
+    
+    // 배열을 ArrayList로 바꾸기
+    Integer[] a = {10, 20, 30, 40, 50};
+    List<Integer> numbers = Arrays.asList(a);
+    
+    // 주의! 초기화된 ArrayList는 길이를 변경(수정,삭제)할 수 없다.
+    // numbers.add(60);   불가능
+    // numbers.remove(0); 불가능    
+    
+   // for문
+    for(int i = 0, length = numbers.size(); i < length; i++) {
+      System.out.println(numbers.get(i));
+    }
+           
+  }
+  
+  public static void ex05() {
+    
+    // ArrayList 초기화
+    List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+    
+    // for문
+    for(int i = 0, length = numbers.size(); i < length; i++) {
+      System.out.println(numbers.get(i));
+    }    
+    
+  }
   public static void main(String[] args) {
     //ex01();
-    ex02();
+    //ex02();
+    //ex03();
+    //ex04();
+    ex05();
   }
 
 }
