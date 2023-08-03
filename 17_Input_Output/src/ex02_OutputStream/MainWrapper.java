39692f42a3002e78f51550ef5ba3c6fc9d7a9949
eylee02 <leywindy@naver.com>
2023-08-03 11:23:16 +0900

    20230803

--- 17_Input_Output/src/ex02_OutputStream/MainWrapper.java
+++ 17_Input_Output/src/ex02_OutputStream/MainWrapper.java
@@ -1,9 +1,11 @@
 package ex02_OutputStream;
 
 import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class MainWrapper {
@@ -139,10 +141,101 @@ public class MainWrapper {
     
   }
   
+  public static void ex04() {
+    
+    /*
+     * java.io.DataOutPutStream 클래스
+     * 1. int, double, String 등의 변수를 그대로 출력하는 출력스트림이다.
+     * 2. 보조스트림이므로 메인스트림과 함께 사용한다.
+     */
+    
+    File dir = new File("C:/storage");
+    if(!dir.exists()) {
+      dir.mkdirs();
+    }
+    
+    File file = new File(dir, "ex04.dat");
+    
+    // 데이터출력스트림 선언
+    DataOutputStream dout = null;
+    
+    try {
+      dout = new DataOutputStream(new FileOutputStream(file));
+      
+      String name = "tom";
+      int age = 50;
+      double height = 180.5;
+      String school = "가산대학교";
+           
+      dout.writeChars(name);
+      dout.writeInt(age);
+      dout.writeDouble(height);
+      dout.writeUTF(school);    // 한글처리
+      
+    } catch(IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+      if(dout != null) {
+        dout.close();
+      }
+      } catch(IOException e) {
+        e.printStackTrace();
+      }
+      System.out.println(file.getPath() + "파일 크기 : " + file.length() + "바이트");
+    }
+
+  }
+  
+  public static void ex05() {
+    
+    /*
+     * java.io.ObjectOutPutStream 클래스
+     * 1. 객체를 그대로 출력하는 출력스트림이다.
+     * 2. 직렬화(Serializable)된 객체를 보낼 수 있다.
+    */
+    
+    File dir = new File("C:/storage");
+    if(!dir.exists()) {
+      dir.mkdirs();
+    }
+    
+    File file = new File(dir, "ex05.dat");
+    
+    // 객체출력스트림 선언
+    ObjectOutputStream oout = null;
+    
+    try {
+      oout = new ObjectOutputStream(new FileOutputStream(file));
+      
+      String name = "tom";
+      int age = 50;
+      double height = 180.5;
+      String school = "가산대학교"; 
+      Student student = new Student(name, age, height, school);
+           
+      oout.writeObject(student);
+      
+    } catch(IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+      if(oout != null) {
+        oout.close();
+      }
+      } catch(IOException e) {
+        e.printStackTrace();
+      }
+      System.out.println(file.getPath() + "파일 크기 : " + file.length() + "바이트");
+    }
+    
+    
+  }
+  
 
 
   public static void main(String[] args) {
-    ex03();
+    ex05();
     
     
   }
